dbbc0f03bd4912174ebce97ac5714123eec0bb02
waanpajaree <applekrnawee@gmail.com>
2023-10-18 13:48:28 +0700

    first commit

---
+++ client/src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Test from '@/components/Test'
+import UserIndex from '@/components/User/Index'
+import UserCreate from '@/components/User/CreateUser'
+import UserEdit from '@/components/User/EditUser'
+import UserShow from '@/components/User/ShowUser'
+import Login from '@/components/Login'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: UserIndex
+    },
+    {
+      path: '/user/create',
+      name: 'user-create',
+      component: UserCreate
+    },
+    {
+      path: '/user/edit/:userId',
+      name: 'user-edit',
+      component: UserEdit
+    },
+    {
+      path: '/user/:userId',
+      name: 'user',
+      component: UserShow
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
+  ]
+})
---
+++ server/src/routes.js
@@ -0,0 +1,31 @@
+const UserController = require('./controllers/UserController')
+const UserAuthenController = require('./controllers/UserAuthenController')
+module.exports = (app) => {
+    
+    // create user
+    app.post('/user',
+        UserController.create
+    )
+
+    // edit user, suspend, active
+    app.put('/user/:userId', UserController.put
+    )
+
+    // delete user
+    app.delete('/user/:userId',
+        UserController.remove
+    )
+
+    // get user by id
+    app.get('/user/:userId',
+        UserController.show
+    )
+    
+    // get all user
+    app.get('/users',
+        UserController.index
+    )
+    // login 
+    app.post('login', 
+    UserAuthenController.login)
+}
